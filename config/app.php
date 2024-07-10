05906222f419adec841f912328eba4dd687b21e8
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-07-11 01:08:21 +0800

    WIP

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Career Training College (CTC)'),
 
     /*
     |--------------------------------------------------------------------------
