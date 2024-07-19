f2f1152925678600a3ad86b715a096fbd9b9b61a
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-07-19 10:29:06 +0800

    WIP

--- resources/views/members/edit.blade.php
+++ resources/views/members/edit.blade.php
@@ -1,12 +1,4 @@
 <x-app-layout>
-    @if(Auth::user()->isAdministrator())
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-indigo-800 leading-tight">
-                {{ __('Edit member') }}
-            </h2>
-        </x-slot>
-    @endif
-
     <div class="py-12">
         <div class="max-w-7xl mx-4 sm:mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-indigo-800 leading-tight">
-            {{ __('Members List') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-4 sm:mx-auto sm:px-6 lg:px-8">
             @if ($message = Session::get('success'))
