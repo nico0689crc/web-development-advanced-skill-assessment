a7da51d324e5a0ff7f2cfaa219ff42e9759cd27d
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-08-06 14:48:08 +0800

    Footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-white border-t border-indigo-100 p-6 flex-shrink">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex flex-col md:flex-row justify-between items-center gap-6">
+          <div class="text-sm font-medium text-indigo-500 text-center">
+              © 2024 Career Training College. All rights reserved.
+          </div>
+          <div class="flex flex-col md:flex-row gap-1 md:gap-4 items-center">
+              <a href="#" class="text-sm font-medium text-indigo-500 hover:text-indigo-700 transition duration-150 ease-in-out">Privacy Policy</a>
+              <a href="#" class="text-sm font-medium text-indigo-500 hover:text-indigo-700 transition duration-150 ease-in-out">Terms of Service</a>
+          </div>
+      </div>
+  </div>
+</footer>
